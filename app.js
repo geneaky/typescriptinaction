7a021eca2dd5a50131734fe2a89ddc977cec4003
geneaky <kaktus418@gmail.com>
2022-06-23 17:41:26 +0900

    mixins via intersections

--- app.js
+++ app.js
@@ -343,4 +343,34 @@ var testResposne = {
     error: { message: 'hi' }
 };
 handleArtistsResponse(testResposne);
+var Person = (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
+var ConsoleLogger = (function () {
+    function ConsoleLogger() {
+    }
+    ConsoleLogger.prototype.log = function (name) {
+        console.log("Hello, I'm ".concat(name, "."));
+    };
+    return ConsoleLogger;
+}());
+function extend(first, second) {
+    var result = {};
+    for (var prop in first) {
+        if (first.hasOwnProperty(prop)) {
+            result[prop] = first[prop];
+        }
+    }
+    for (var prop in second) {
+        if (second.hasOwnProperty(prop)) {
+            result[prop] = second[prop];
+        }
+    }
+    return result;
+}
+var jim = extend(new Person("Jim"), ConsoleLogger.prototype);
+jim.log(jim.name);
 //# sourceMappingURL=app.js.map
